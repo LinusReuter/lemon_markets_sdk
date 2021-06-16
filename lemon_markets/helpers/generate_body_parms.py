d296ffa1eeda4270ab9fc9f6da14400e64b2ddbf
Linus Reuter <linus.reuter@icloud.com>
2021-06-16 15:13:27 +0200

    deleted unused class

--- lemon_markets/helpers/generate_body_parms.py
+++
@@ -1,8 +0,0 @@
-class Body:
-
-    def _build_body(self) -> dict:
-        primitive_types = (float, int, str, bool)
-        body = {}
-        for attribute, value in self.__dict__.items():
-            print(attribute, value)
-
