cdef21509498e72632c474f6973a10da573aaec1
Linus Reuter <linus.reuter@icloud.com>
2021-07-20 00:34:43 +0200

    completed trading venue

--- lemon_markets/helpers/time_helper.py
+++ lemon_markets/helpers/time_helper.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from datetime import timedelta
 from typing import Union
 
 
--- lemon_markets/paper_money/trading_venue.py
+++ lemon_markets/paper_money/trading_venue.py
@@ -2,36 +2,7 @@ from dataclasses import dataclass
 
 from lemon_markets.helpers.api_client import ApiClient
 from lemon_markets.account import Account
-
-@dataclass()
-class TradingVenue:
-    name: str = None
-    title: str = None
-    mic: str = None
-    opening_days: list = None
-    request_class: object = None
-
-    @classmethod
-    def from_response(cls, request_class, data: dict):
-
-        return cls(
-            request_class=request_class,
-            name=data.get('name'),
-            title=data.get('title'),
-            mic=data.get('mic')
-        )
-
-    @property
-    def is_open(self):
-        return None
-
-    @property
-    def time_until_close(self):
-        return None
-
-    @property
-    def time_until_open(self):
-        return None
+from lemon_markets.helpers.time_helper import *
 
 
 class TradingVenues(ApiClient):
@@ -47,3 +18,83 @@ class TradingVenues(ApiClient):
     def get_opening_days(self, mic):
         return self._request(endpoint=f"trading-venues/{mic}/opening-days")
 
+
+@dataclass()
+class TradingVenue:
+    name: str = None
+    title: str = None
+    mic: str = None
+    opening_days: list = None
+    request_class: TradingVenues = None
+
+    @classmethod
+    def from_response(cls, request_class, data: dict):
+
+        return cls(
+            request_class=request_class,
+            name=data.get('name'),
+            title=data.get('title'),
+            mic=data.get('mic')
+        )
+
+    @property
+    def is_open(self):
+        day = current_time().strftime("%Y-%m-%d")
+        if self.opening_days is None:
+            self.get_opening_days()
+
+        for data in self.opening_days:
+            if day == data.get('day_iso'):
+                if timestamp_to_datetime(data.get("opening_time")) <= current_time() <= timestamp_to_datetime(
+                        data.get("closing_time")):
+                    return True
+                else:
+                    return False
+
+        self.get_opening_days()
+        for data in self.opening_days:
+            if day == data.get('day_iso'):
+                if timestamp_to_datetime(data.get("opening_time")) <= current_time() <= timestamp_to_datetime(
+                        data.get("closing_time")):
+                    return True
+                else:
+                    return False
+
+        return False
+
+    @property
+    def time_until_close(self):
+        day = current_time().strftime("%Y-%m-%d")
+        if self.opening_days is None:
+            self.get_opening_days()
+
+        for data in self.opening_days:
+            if day == data.get('day_iso'):
+                return timestamp_to_datetime(data.get("closing_time")) - current_time()
+
+        self.get_opening_days()
+        for data in self.opening_days:
+            if day == data.get('day_iso'):
+                return timestamp_to_datetime(data.get("closing_time")) - current_time()
+
+        return timedelta()
+
+    @property
+    def time_until_open(self):
+        day = current_time().strftime("%Y-%m-%d")
+        if self.opening_days is None:
+            self.get_opening_days()
+
+        for data in self.opening_days:
+            if day == data.get('day_iso'):
+                return timestamp_to_datetime(data.get("opening_time")) - current_time()
+
+        self.get_opening_days()
+        for data in self.opening_days:
+            if day == data.get('day_iso'):
+                return timestamp_to_datetime(data.get("opening_time")) - current_time()
+
+        return timedelta()
+
+    def get_opening_days(self):
+        self.opening_days = self.request_class.get_opening_days(self.mic)
