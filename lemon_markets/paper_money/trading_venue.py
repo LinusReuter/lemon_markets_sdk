fd4332576a6f2c9a739fc668d5c870bf9465f8c3
Linus Reuter <linus.reuter@icloud.com>
2021-07-19 03:30:16 +0200

    trading venue request

--- lemon_markets/paper_money/trading_venue.py
+++ lemon_markets/paper_money/trading_venue.py
@@ -9,7 +9,29 @@ class TradingVenue:
     title: str = None
     mic: str = None
     opening_days: list = None
+    request_class: object = None
 
+    @classmethod
+    def from_response(cls, request_class, data: dict):
+
+        return cls(
+            request_class=request_class,
+            name=data.get('name'),
+            title=data.get('title'),
+            mic=data.get('mic')
+        )
+
+    @property
+    def is_open(self):
+        return None
+
+    @property
+    def time_until_close(self):
+        return None
+
+    @property
+    def time_until_open(self):
+        return None
 
 
 class TradingVenues(ApiClient):
@@ -17,3 +39,11 @@ class TradingVenues(ApiClient):
 
     def __init__(self, account: Account):
         super().__init__(account=account)
+
+    def get_venues(self):
+        data_rows = self._request_paged('trading-venues/')
+        self.trading_venues = [TradingVenue.from_response(self, data) for data in data_rows]
+
+    def get_opening_days(self, mic):
+        return self._request(endpoint=f"trading-venues/{mic}/opening-days")
+
