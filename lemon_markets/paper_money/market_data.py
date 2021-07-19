04b5e7e3cd3f7e719f7550655b20b2156f146876
Linus Reuter <linus.reuter@icloud.com>
2021-07-20 01:52:52 +0200

    OHLC Data

--- lemon_markets/paper_money/market_data.py
+++ lemon_markets/paper_money/market_data.py
@@ -1,4 +1,27 @@
 from dataclasses import dataclass
+from datetime import datetime
+
+from lemon_markets.helpers.api_client import ApiClient
+from lemon_markets.account import Account
+from lemon_markets.helpers.time_helper import datetime_to_timestamp
+from lemon_markets.paper_money.instrument import Instrument
+from lemon_markets.paper_money.trading_venue import TradingVenue
+
 
 @dataclass()
-class OHLCData:
+class OHLC(ApiClient):
+
+    def __init__(self, account: Account):
+        super().__init__(account=account)
+
+    def get_data(self, instrument: Instrument, venue: TradingVenue, X1: str, ordering: str = None,
+                 date_from: datetime = None, date_until: datetime = None):
+        endpoint = f"trading-venues/{TradingVenue.mic}/instruments/{Instrument.isin}/data/ohlc/{X1}/"
+        params = {}
+        if ordering is not None:
+            params['ordering'] = ordering
+        if date_from is not None:
+            params['date_from'] = int(datetime_to_timestamp(date_from))
+        if date_until is not None:
+            params['date_until'] = int(datetime_to_timestamp(date_until))
+        return self._request_paged(endpoint=endpoint, params=params)
