df136fa9782254bc2892f71caef6ba18cea10f69
Linus Reuter <linus.reuter@icloud.com>
2021-08-03 12:41:13 +0200

    preparation for pip release

--- setup.py
+++ setup.py
@@ -2,14 +2,25 @@ from lemon_markets import __version__
 from setuptools import setup, find_packages
 
 if __name__ == '__main__':
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
     setup(
-        name='lemon_markets',
+        name='lemon_markets_sdk',
         version=__version__,
-        description='Lemon Markets API Access SDK',
+        description='SDK for Lemon Markets API',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
         license='MIT',
         python_requires='>=3.6',
         author='Linus Reuter',
+        url="https://github.com/LinusReuter/lemon-markets-api-access",
         packages=find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
         install_requires=[
             'requests',
         ],
